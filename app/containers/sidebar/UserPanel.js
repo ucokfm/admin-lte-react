7805d454f842f67a8cbd4a2bb9b085d7d17cb3ea
Ucok Freddy <ucokfm@gmail.com>
2016-07-12 10:12:32 +0700

    add statusIcon and statusText props to UserPanel

--- app/containers/sidebar/UserPanel.js
+++ app/containers/sidebar/UserPanel.js
@@ -7,15 +7,20 @@ const user = {
   title: 'Web Developer',
   joined: 'Nov. 2012',
   avatar: 'node_modules/admin-lte/dist/img/user2-160x160.jpg',
-  online: true,
+  isOnline: true,
 };
 
 export default function () {
+  const onlineIcon = 'fa fa-circle text-success';
+  const offlineIcon = 'fa fa-circle text-danger';
+  const statusIcon = user.isOnline ? onlineIcon : offlineIcon;
+  const statusText = user.isOnline ? 'Online' : 'Offline';
   return (
     <UserPanel
       image={user.avatar}
       name={user.name}
-      online={user.online}
+      statusIcon={statusIcon}
+      statusText={statusText}
     />
   );
 }
